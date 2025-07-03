037be94eb033ad90f3aa39de45721113d722f622
denilsontorres <dtlima2020@gmail.com>
2025-07-03 11:07:31 -0300

    feat: logic to display cart, delete item, subtotal added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+import * as cartServices from "./services/cart.js";
 import createItem from "./services/item.js";
 
-const cart = [];
+const myCart = [];
+const myWishList = [];
 
 console.log("Welcome to the your Shopee Cart!");
 
 const item1 = await createItem("hotwheels ferrari", 20.99, 1);
 const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
 
-console.log(item2.subtotal());
+await cartServices.addItemToCart(myCart, item1);
+await cartServices.addItemToCart(myCart, item2);
+await cartServices.displayCart(myCart);
+
+// await cartServices.delItemToCart(myCart, item2.name);
+// await cartServices.delItemToCart(myCart, item1.name);
+
+
+await cartServices.calculateTotal(myCart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -1,11 +1,42 @@
 // adicionar item
-async function addItemToCart(userCart) {}
+async function addItemToCart(userCart, item) {
+  userCart.push(item);
+}
 
 // deletar item
-async function delItemToCart(userCart, name) {}
+async function delItemToCart(userCart, name) {
+  const index = userCart.findIndex((item) => item.name === name);
+
+  if (index !== -1) {
+    userCart.splice(index, 1);
+  }
+}
 
 // remover -1 item
 async function removeItemToCart(userCart, index) {}
 
 // calcular total
-async function calculateTotal(userCart) {}
+async function calculateTotal(userCart) {
+  const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+  console.log(" \nShopee Cart TOTAL is: ");
+  console.log(`Total: ${result}`);
+}
+
+async function displayCart(userCart) {
+  console.log("\nShopee Cart list ");
+  userCart.forEach((item, index) => {
+    console.log(
+      `${index + 1}. ${item.name} - R$ ${item.price} | ${
+        item.quantity
+      } | Subtotal ${item.subtotal()}`
+    );
+  });
+}
+
+export {
+  addItemToCart,
+  delItemToCart,
+  removeItemToCart,
+  calculateTotal,
+  displayCart,
+};
